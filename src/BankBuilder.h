3ec485a0ed79fdab71bdcd78337e73ab4718183c
Aaron Ishibashi <a.ishibashi.music@gmail.com>
2023-10-16 15:03:50 -0700

    Add BankBuilder class

---
+++ src/BankBuilder.h
@@ -0,0 +1,62 @@
+#pragma once
+
+#include "common.h"
+#include <cstdlib>
+
+namespace Insound {
+
+    class BankBuilder {
+    public:
+        BankBuilder();
+        ~BankBuilder();
+
+        static Result initLibrary() noexcept;
+        static Result closeLibrary() noexcept;
+
+        /**
+         * Add a file pointer to the bank. Do not delete the file until you are
+         * done with Bank functionality, as it is not copied.
+         *
+         * @param file      the file pointer to save with the bank.
+         */
+        Result addFile(void *data, unsigned size) noexcept;
+
+        /**
+         * Compile the bank into an fsbank, saving it at the location.
+         *
+         * A successful call to Bank::initLibrary must be made before running
+         * this function.
+         *
+         * Make sure to consume the file via Bank::getLength and Bank::getFile
+         * right after this call, since the next call to any other Bank::build
+         * will overwrite it.
+         *
+         * @param samplerate    the desired samplerate, default: 44100
+         *
+         */
+        Result build(float samplerate = 44100.f) noexcept;
+
+        /**
+         * Clear internals for object reuse.
+         */
+        Result clear() noexcept;
+
+        /**
+         * Get the data of the built file. Only available after a successful
+         * call to BankBuilder::build.
+         */
+        const std::vector<uint8_t> &data() const noexcept { return builtFile; }
+
+    private:
+        // Byte length of each file
+        std::vector<unsigned> fileSizes;
+
+        // File pointers
+        std::vector<void *> files;
+
+        // Data of the built file, only available after a successful call to
+        // BankBuilder::build.
+        std::vector<uint8_t> builtFile;
+    };
+
+}
