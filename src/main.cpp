fc20024a6c8e9456c1ca44a97d15353950d19c3b
Aaron Ishibashi <a.ishibashi.music@gmail.com>
2023-10-15 21:54:05 -0700

    Stub up test in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,28 @@
 #include <iostream>
 #include <crow.h>
+#include <fsbank.h>
+#include <fsbank_errors.h>
+
+#include <thread>
+
+#define FSB_CHECK(result) do { \
+        const FSBANK_RESULT res = (result); \
+        if ( (res) != FSBANK_OK ) { \
+            std::cerr << FSBank_ErrorString(res); \
+            return (res); \
+        } \
+    } while(0)
+
+
 
 int main (int argc, char *argv[])
 {
-    std::cout << "Hello fsbank server!\n";
+    const auto NumCores = std::thread::hardware_concurrency();
+    FSB_CHECK( FSBank_Init(FSBANK_FSBVERSION_FSB5, FSBANK_INIT_NORMAL, NumCores, ".fscache") );
+    std::cout << "FSBank Initialized.\n";
+
+    // Do stuff here
+
+    FSB_CHECK ( FSBank_Release() );
+    std::cout << "FSBank Released.\n";
 }
