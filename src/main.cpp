b29b131ccb86cfc3e0b9141d2e9a30fdaea542d5
Aaron Ishibashi <a.ishibashi.music@gmail.com>
2023-10-16 15:02:12 -0700

    Get port number from the environment, remove fsb functions from main

--- src/main.cpp
+++ src/main.cpp
@@ -7,34 +7,19 @@
 #include "server.h"
 #include <cstdlib>
 
-
-#define FSB_CHECK(result) do { \
-        const FSBANK_RESULT res = (result); \
-        if ( (res) != FSBANK_OK ) { \
-            std::cerr << FSBank_ErrorString(res); \
-            return (res); \
-        } \
-    } while(0)
-
 const int DefaultPort = 1234;
 
 int main (int argc, char *argv[])
 {
-    const auto NumCores = std::thread::hardware_concurrency();
-    FSB_CHECK( FSBank_Init(FSBANK_FSBVERSION_FSB5, FSBANK_INIT_NORMAL, NumCores, ".fscache") );
-    std::cout << "FSBank Initialized.\n";
-
-    // Do stuff here
-
-    FSB_CHECK ( FSBank_Release() );
-    std::cout << "FSBank Released.\n";
-
+    // Get the PORT from environment
     int PORT;
     if (auto portEnv = std::getenv("PORT"))
     {
         try {
             PORT = std::atoi(portEnv);
         } catch (...) {
+            std::cerr << "Invalid PORT: not a number. Reverting to default " <<
+                DefaultPort << '\n';
             PORT = DefaultPort;
         }
     }
