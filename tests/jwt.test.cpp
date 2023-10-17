564aafff9f7900ab2c1c576ee86a29535dfb0ce9
Aaron Ishibashi <a.ishibashi.music@gmail.com>
2023-10-17 16:53:39 -0700

    Update jwt tests to reflect expiration feature

--- tests/jwt.test.cpp
+++ tests/jwt.test.cpp
@@ -2,6 +2,10 @@
 #include <insound/jwt.h>
 #include <catch2/catch_test_macros.hpp>
 
+#include <insound/chrono.h>
+
+using namespace Insound::ChronoLiterals;
+
 TEST_CASE ("JWT serialize & deserialize", "[Jwt]")
 {
     // Configure the JWT_SECRET test variable
@@ -14,15 +18,25 @@ TEST_CASE ("JWT serialize & deserialize", "[Jwt]")
         json.set("age", 42);
 
         std::string jwt;
-        REQUIRE_NOTHROW(jwt = Insound::Jwt::serialize(json));
+        REQUIRE_NOTHROW(jwt = Insound::Jwt::sign(json, 14_d)); // 14 day expiry
 
         REQUIRE(!jwt.empty());
 
         Poco::JSON::Object res;
-        REQUIRE_NOTHROW(res = Insound::Jwt::deserialize(jwt));
+        REQUIRE_NOTHROW(res = Insound::Jwt::verify(jwt));
 
         REQUIRE (json.get("name").toString() == res.get("name").toString());
         REQUIRE (json.get("age").convert<int>() == res.get("age").convert<int>());
+    }
+
+    SECTION("expired token throws")
+    {
+        std::string jwt;
+        REQUIRE_NOTHROW(jwt = Insound::Jwt::sign(Poco::JSON::Object(), -1_s));
+
+        REQUIRE(!jwt.empty());
+
+        REQUIRE_THROWS(Insound::Jwt::verify(jwt));
     }
 
 }
