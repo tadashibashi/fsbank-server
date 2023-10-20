4a590ba1a1918515a6500c42320b3fe9615b401c
Aaron Ishibashi <a.ishibashi.music@gmail.com>
2023-10-20 04:26:09 -0700

    Forgot to add fmt.hpp header

---
+++ insound/thirdparty/fmt.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <spdlog/fmt/fmt.h>
+
+/**
+ * Python f-string using fmt library
+ */
+template <typename... T>
+FMT_NODISCARD FMT_INLINE auto f(fmt::format_string<T...> format,
+    T&&... args) -> std::string {
+    return fmt::vformat(format, fmt::make_format_args(args...));
+}
