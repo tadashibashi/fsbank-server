56eb1972a13722328f96a1898fe9d581031560a1
Aaron Ishibashi <a.ishibashi.music@gmail.com>
2023-10-19 21:07:02 -0700

    Add comments to password.h

--- insound/password.h
+++ insound/password.h
@@ -4,9 +4,17 @@
 
 namespace Insound {
 
+    /**
+     * Encrypt a text string
+     */
     [[nodiscard]]
     std::string hash(const std::string &text);
 
+
+    /**
+     * Compare text to a string that was encrypted via Insound::hash
+     * @returns true when text matches encrypted string, and false otherwise.
+     */
     [[nodiscard]]
     bool compare(const std::string &text, const std::string &encrypted);
 }
