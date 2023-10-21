67c3af9cb11bb2a7351b7905c43d0417eb5eab8e
Aaron Ishibashi <a.ishibashi.music@gmail.com>
2023-10-21 14:33:19 -0700

    Add mongo config test

---
+++ insound/mongo.cpp
@@ -0,0 +1,43 @@
+#include "mongo.h"
+
+#include <insound/env.h>
+
+#include <bsoncxx/json.hpp>
+#include <mongocxx/client.hpp>
+#include <mongocxx/instance.hpp>
+
+namespace Insound::Mongo {
+    static mongocxx::instance sInstance{};
+    static mongocxx::client sClient;
+
+    static mongocxx::database sDB;
+
+    mongocxx::database &db() {
+        return sDB;
+    }
+
+    bool connect()
+    {
+        try {
+            sClient = mongocxx::client(
+            mongocxx::uri(requireEnv("MONGO_URL")));
+            sDB = sClient[requireEnv("MONGO_DBNAME")];
+
+            auto users = sDB.collection("users");
+            auto user = users.find_one({});
+            IN_LOG(bsoncxx::to_json(user.value()));
+
+            return true;
+        }
+        catch (const std::exception &e)
+        {
+            IN_ERR("Error during db init: {}", e.what());
+            return false;
+        }
+        catch (...)
+        {
+            IN_ERR("Error occurred during db init.");
+            return false;
+        }
+    }
+}
--- insound/mongo.h
+++ insound/mongo.h
@@ -1,1 +1,9 @@
+#pragma once
+#include <mongocxx/database.hpp>
 
+namespace Insound::Mongo
+{
+    bool connect();
+
+    mongocxx::database &db();
+}
