9e863d33a1eb47aa67267e0add8482403cfa2537
Aaron Ishibashi <a.ishibashi.music@gmail.com>
2023-10-22 04:23:18 -0700

    Create GLZ_FORMAT macro, easily specialize fmt for structs that specialize glz::meta

---
+++ insound/format.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <insound/errors/GlazeError.h>
+#include <insound/thirdparty/fmt.hpp>
+#include <insound/thirdparty/glaze.hpp>
+
+#include <glaze/util/type_traits.hpp>
+
+/**
+ * Pass a type that specializes glz::meta in order to auto-format it
+ * to a JSON string when passed into fmt library, f-strings, logs, etc.
+ * @param  T - the type to format
+ */
+#define GLZ_FORMAT(T) template<> \
+    struct fmt::formatter<T> { \
+        template<typename ParseContext> \
+        constexpr auto parse(ParseContext& ctx) { \
+            return ctx.begin(); \
+        } \
+        template<typename FormatContext> \
+        auto format(T const& obj, FormatContext& ctx) { \
+            auto buffer = glz::write_json(obj); \
+            return fmt::format_to(ctx.out(), "{}", buffer); \
+        } \
+    }
