1ade10dc61312d9a88c2e9a49caa3d07b706336b
Aaron Ishibashi <a.ishibashi.music@gmail.com>
2023-10-21 23:27:12 -0700

    Update password implementation to use bcrypt instead of Poco

--- insound/password.cpp
+++ insound/password.cpp
@@ -1,43 +1,18 @@
 #include "password.h"
-#include "Poco/Crypto/Cipher.h"
-#include "Poco/Crypto/CipherFactory.h"
-#include "Poco/Crypto/CipherKey.h"
 #include <insound/env.h>
 #include <insound/log.h>
-
+#include <bcrypt.h>
 #include <memory>
-
-using Poco::Crypto::Cipher;
-using Poco::Crypto::CipherFactory;
-using Poco::Crypto::CipherKey;
 
 namespace Insound {
 
-    static Cipher &getCipher()
-    {
-        static std::shared_ptr<Cipher> sCipher;
-        if (!sCipher)
-        {
-            auto &factory = CipherFactory::defaultFactory();
-            auto password = requireEnv("PASSWORD_SECRET");
-            auto salt = requireEnv("PASSWORD_SALT");
-
-            auto key = CipherKey("aes-256-cbc",
-                Cipher::ByteVec{password.begin(), password.end()},
-                Cipher::ByteVec{salt.begin(), salt.end()});
-            sCipher = std::shared_ptr<Cipher>(factory.createCipher(key));
-        }
-
-        return *sCipher;
-    }
-
     std::string hash(const std::string &text)
     {
-        return getCipher().encryptString(text);
+        return bcrypt::generateHash(text);
     }
 
     bool compare(const std::string &text, const std::string &encrypted)
     {
-        return getCipher().decryptString(encrypted) == text;
+        return bcrypt::validatePassword(text, encrypted);
     }
 }
