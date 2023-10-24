77dd2ad7a27915c9ceba1af00393e107e5f468f1
Aaron Ishibashi <a.ishibashi.music@gmail.com>
2023-10-24 05:06:36 -0700

    remove endpoint for causing type error

--- insound/thirdparty/crow.hpp
+++ insound/thirdparty/crow.hpp
@@ -1,6 +1,6 @@
 #pragma once
 #include <insound/definitions.h>
 #define CROW_STATIC_DIRECTORY PUBLIC_DIR
-#define CROW_STATIC_ENDPOINT "/public"
+//#define CROW_STATIC_ENDPOINT "/public"
 
 #include <crow.h>
