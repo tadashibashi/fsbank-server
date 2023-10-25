d8d32cfe50619fe0aafc55de231e88f8b7d6f4c6
Aaron Ishibashi <a.ishibashi.music@gmail.com>
2023-10-25 04:51:53 -0700

    Test run s3 functions in server

--- insound/server/Server.cpp
+++ insound/server/Server.cpp
@@ -1,10 +1,14 @@
 #include "Server.h"
+#include "insound/core/s3.h"
 #include <insound/server/routes/api/auth.h>
 
 namespace Insound {
     bool Server::init() {
         mount<Insound::Auth>();
 
+        S3::config();
+        auto files = S3::listObjects();
+        IN_LOG("File size: {}", files.size());
         return true;
     }
 }
