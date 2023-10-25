6f68e5fc81e177fcbd29432d2522f3a6ebcb6de4
Aaron Ishibashi <a.ishibashi.music@gmail.com>
2023-10-25 16:55:55 -0700

    Add `prefix` param to listObjects

--- insound/core/s3.cpp
+++ insound/core/s3.cpp
@@ -43,11 +43,12 @@ namespace Insound::S3 {
         return client;
     }
 
-    std::vector<std::string> listObjects()
+    std::vector<std::string> listObjects(const std::string_view &prefix)
     {
         auto client = getClient();
         auto request = Aws::S3::Model::ListObjectsV2Request();
         request.SetBucket(S3_BUCKET);
+        request.SetPrefix(prefix.data());
 
         auto result = client.ListObjectsV2(request);
         auto objects = result.GetResult().GetContents();
--- insound/core/s3.h
+++ insound/core/s3.h
@@ -6,10 +6,22 @@ namespace Insound::S3 {
     bool config();
     void close();
 
-    std::vector<std::string> listObjects();
+    /**
+     * Get a list of object keys in the store, stemming from `prefix`
+     * @param   prefix - the prefix from which to stem the search for objects
+     *
+     * @returns the list of key names
+     */
+    std::vector<std::string> listObjects(const std::string_view &prefix = "");
 
-    // bool uploadFile(const std::string_view &key,
-    //     const std::vector<uint8_t> &file);
+    /**
+     * Upload a file to the project's s3 bucket
+     * @param  key  [description]
+     * @param  file [description]
+     * @return      [description]
+     */
+    bool uploadFile(const std::string_view &key,
+        const std::vector<uint8_t> &file);
     // std::vector<uint8_t> downloadFile(const std::string_view &key);
 
 }
