414435ad8814b8746bcf3d4b478cb058407c6e96
Aaron Ishibashi <a.ishibashi.music@gmail.com>
2023-10-26 03:49:35 -0700

    Remove some S3 tests from Server

--- insound/server/Server.cpp
+++ insound/server/Server.cpp
@@ -1,14 +1,13 @@
 #include "Server.h"
-#include "insound/core/s3.h"
+#include <insound/core/s3.h>
 #include <insound/server/routes/api/auth.h>
 
 namespace Insound {
     bool Server::init() {
-        mount<Insound::Auth>();
+        mount<Auth>();
 
         S3::config();
-        auto files = S3::listObjects();
-        IN_LOG("File size: {}", files.size());
+
         return true;
     }
 }
