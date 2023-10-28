43fd1ec073875569938dfc2c1ad891f617342c7d
Aaron Ishibashi <a.ishibashi.music@gmail.com>
2023-10-27 18:48:54 -0700

    Add multithreaded tests to BankBuilder

--- insound/tests/insound/BankBuilder.test.cpp
+++ insound/tests/insound/BankBuilder.test.cpp
@@ -3,6 +3,7 @@
 #include <insound/tests/test.h>
 #include <insound/core/BankBuilder.h>
 #include <fstream>
+#include <thread>
 
 static std::string openFile(const std::string &path)
 {
@@ -22,16 +23,18 @@ static std::string openFile(const std::string &path)
     return buffer;
 }
 
-
+static std::string file1;
+static std::string file2;
+static std::string file3;
 
 class BankTest {
 public:
-    BankTest() :
-        file1(openFile(PUBLIC_DIR "/audio/test.mp3")),
-        file2(openFile(PUBLIC_DIR "/audio/test.ogg")),
-        file3(openFile(PUBLIC_DIR "/audio/test.wav"))
+    BankTest()
     {
         REQUIRE(BankBuilder::initLibrary() == nullptr);
+        file1 = openFile(PUBLIC_DIR "/audio/test.mp3");
+        file2 = openFile(PUBLIC_DIR "/audio/test.ogg");
+        file3 = openFile(PUBLIC_DIR "/audio/test.wav");
     }
 
     ~BankTest()
@@ -39,9 +42,7 @@ public:
         REQUIRE(BankBuilder::closeLibrary() == nullptr);
     }
 
-    std::string file1;
-    std::string file2;
-    std::string file3;
+
 };
 
 TEST_CASE_METHOD(BankTest, "Files have substance")
@@ -59,4 +60,58 @@ TEST_CASE_METHOD(BankTest, "Bank can build")
     bank.addFile(file3.data(), file3.size());
     REQUIRE(bank.build() == nullptr);
     REQUIRE(!bank.data().empty());
+}
+
+
+void addToVector(std::vector<std::vector<uint8_t>> &files, const std::vector<uint8_t> &file)
+{
+    static std::mutex lock;
+    lock.lock();
+
+    files.emplace_back(file);
+
+    lock.unlock();
+}
+
+TEST_CASE_METHOD(BankTest, "Bank can build in multithreaded context")
+{
+    const auto NumThreads = 100;
+    std::vector<std::thread> threads;
+    std::vector<std::vector<uint8_t>> files;
+
+    // Build 100 banks simultaneously
+    for (int i = 0; i < NumThreads; ++i)
+    {
+        threads.emplace_back([&files]() {
+            BankBuilder bank;
+            bank.addFile(file1.data(), file1.size());
+            bank.addFile(file2.data(), file2.size());
+            bank.addFile(file3.data(), file3.size());
+            REQUIRE(bank.build() == nullptr);
+            REQUIRE(!bank.data().empty());
+            addToVector(files, bank.data());
+        });
+    }
+
+    // await threads
+    for (int i = 0; i < NumThreads; ++i)
+    {
+        threads[i].join();
+    }
+
+    REQUIRE(!files.empty());
+    REQUIRE(files.size() == NumThreads);
+
+    bool allFilesMatch = true;
+    // check if collected data matches
+    for (int i = 0; i < NumThreads; ++i)
+    {
+        if (files[0] != files[i])
+        {
+            allFilesMatch = false;
+            break;
+        }
+    }
+
+    REQUIRE(allFilesMatch);
 }
