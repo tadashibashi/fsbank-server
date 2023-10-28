6f703ef70b4b82c52325f2b4afc1b484ab3448bb
Aaron Ishibashi <a.ishibashi.music@gmail.com>
2023-10-28 04:33:54 -0700

    Set up template install & local dir

--- insound/server/Server.cpp
+++ insound/server/Server.cpp
@@ -1,9 +1,12 @@
 #include "Server.h"
 #include <insound/core/s3.h>
 #include <insound/server/routes/api/auth.h>
+#include <insound/core/definitions.h>
 
 namespace Insound {
     bool Server::init() {
+        crow::mustache::set_global_base(TEMPLATE_DIR);
+
         mount<Auth>();
 
         S3::config();
