889da128178be1cb072bf3cd520bf6b0694b65f9
Aaron Ishibashi <a.ishibashi.music@gmail.com>
2023-10-28 06:18:31 -0700

    Implement catchall route with template

--- insound/server/Server.cpp
+++ insound/server/Server.cpp
@@ -1,15 +1,22 @@
 #include "Server.h"
+
+#include <insound/core/definitions.h>
+#include <insound/core/mongo.h>
 #include <insound/core/s3.h>
 #include <insound/server/routes/api/auth.h>
-#include <insound/core/definitions.h>
 
 namespace Insound {
-    bool Server::init() {
+    bool Server::init()
+    {
         crow::mustache::set_global_base(TEMPLATE_DIR);
+        S3::config();
+        Mongo::connect();
 
         mount<Auth>();
 
-        S3::config();
+        CROW_CATCHALL_ROUTE(this->internal())([]() {
+            return crow::mustache::load("index.html");
+        });
 
         return true;
     }
