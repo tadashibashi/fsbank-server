62f73844044384c67c0d36676dccc8d78274baea
Aaron Ishibashi <a.ishibashi.music@gmail.com>
2023-10-27 19:07:58 -0700

    Organize & comment BankBuilder tests

--- insound/tests/insound/BankBuilder.test.cpp
+++ insound/tests/insound/BankBuilder.test.cpp
@@ -1,34 +1,43 @@
-#include "catch2/catch_test_macros.hpp"
+#include <insound/core/BankBuilder.h>
 #include <insound/tests/definitions.h>
 #include <insound/tests/test.h>
-#include <insound/core/BankBuilder.h>
+
 #include <fstream>
 #include <thread>
 
-static std::string openFile(const std::string &path)
+
+// ===== Helper function declarations =========================================
+
+/**
+ * Helper function for this test - opens binary files into strings
+ *
+ * @param  path - path to the file
+ *
+ * @returns     the file data wrapped in a string
+ */
+static std::string openFile(const std::string &path);
+
+/**
+ * Thread-safe helper to add a file string to a files vector.
+ *
+ * @param files - files list
+ * @param file  - file to add
+ */
+static void addToVector(std::vector<std::vector<uint8_t>> &files,
+    const std::vector<uint8_t> &file);
+
+
+// ===== Tests ================================================================
+
+class BankTest
 {
-    std::ifstream file(path);
-    if (!file.is_open())
-    {
-        throw std::runtime_error("Failed to open file at path " + path);
-    }
-
-    file.seekg(0, std::ios::end);
-    auto filesize = file.tellg();
-    std::string buffer(filesize, ' ');
-    file.seekg(0);
-    file.read(buffer.data(), filesize);
-
-    file.close();
-    return buffer;
-}
-
-static std::string file1;
-static std::string file2;
-static std::string file3;
-
-class BankTest {
 public:
+    // Test file data
+    inline static std::string file1{};
+    inline static std::string file2{};
+    inline static std::string file3{};
+
+    // Setup
     BankTest()
     {
         REQUIRE(BankBuilder::initLibrary() == nullptr);
@@ -37,13 +46,16 @@ public:
         file3 = openFile(PUBLIC_DIR "/audio/test.wav");
     }
 
+    // Tear down
     ~BankTest()
     {
         REQUIRE(BankBuilder::closeLibrary() == nullptr);
+        file1.clear();
+        file2.clear();
+        file3.clear();
     }
-
-
 };
+
 
 TEST_CASE_METHOD(BankTest, "Files have substance")
 {
@@ -51,6 +63,7 @@ TEST_CASE_METHOD(BankTest, "Files have substance")
     REQUIRE(!file2.empty());
     REQUIRE(!file3.empty());
 }
+
 
 TEST_CASE_METHOD(BankTest, "Bank can build")
 {
@@ -62,16 +75,6 @@ TEST_CASE_METHOD(BankTest, "Bank can build")
     REQUIRE(!bank.data().empty());
 }
 
-
-void addToVector(std::vector<std::vector<uint8_t>> &files, const std::vector<uint8_t> &file)
-{
-    static std::mutex lock;
-    lock.lock();
-
-    files.emplace_back(file);
-
-    lock.unlock();
-}
 
 TEST_CASE_METHOD(BankTest, "Bank can build in multithreaded context")
 {
@@ -114,4 +117,36 @@ TEST_CASE_METHOD(BankTest, "Bank can build in multithreaded context")
     }
 
     REQUIRE(allFilesMatch);
+}
+
+
+// ===== Helper function definitions ==========================================
+
+std::string openFile(const std::string &path)
+{
+    std::ifstream file(path, std::ios::binary | std::ios::binary);
+    if (!file.is_open())
+    {
+        throw std::runtime_error("Failed to open file at path " + path);
+    }
+
+    file.seekg(0, std::ios::end);
+    auto filesize = file.tellg();
+    std::string buffer(filesize, ' ');
+    file.seekg(0);
+    file.read(buffer.data(), filesize);
+
+    file.close();
+    return buffer;
+}
+
+void addToVector(std::vector<std::vector<uint8_t>> &files,
+    const std::vector<uint8_t> &file)
+{
+    static std::mutex lock;
+    lock.lock();
+
+    files.emplace_back(file);
+
+    lock.unlock();
 }
