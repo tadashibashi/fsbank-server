0d25b8abe05d2e47be7382abe79671c94b055ee2
Aaron Ishibashi <a.ishibashi.music@gmail.com>
2023-11-02 04:54:41 -0700

    Make Helmet middleware configurable

--- insound/core/middleware/Helmet.cpp
+++ insound/core/middleware/Helmet.cpp
@@ -1,44 +1,82 @@
 #include "Helmet.h"
+#include <insound/core/env.h>
 #include <insound/core/util.h>
 
 void Insound::Helmet::before_handle(crow::request &req,
     crow::response &res, context &ctx)
 {
-    res.add_header("Strict-Transport-Security",
-        "max-age=15552000; includeSubDomains");
-    res.add_header("Cross-Origin-Embedder-Policy", "require-corp");
-    res.add_header("Cross-Origin-Opener-Policy", "same-origin");
-    res.add_header("Cross-Origin-Resource-Policy", "same-origin");
-    res.add_header("Cross-Agent-Cluster", "?1");
-    res.add_header("Referrer-Policy", "no-referrer");
-    res.add_header("X-Content-Type-Options", "no-sniff");
-    res.add_header("X-DNS-Prefetch-Control", "off");
-    res.add_header("X-Download-Options", "noopen");
-    res.add_header("X-Frame-Options", "SAMEORIGIN");
-    res.add_header("X-Permitted-Cross-Domain-Policies", "none");
-    res.add_header("X-XSS-Protection", "0");
-
-    // set nonce
+    // Create nonce
     auto nonce = genBytes(16);
     auto nonceStr = crow::utility::base64encode(nonce.data(), nonce.size());
-
     ctx.nonce = nonceStr;
-    res.add_header("Content-Security-Policy",
-        f(
-            "base-uri 'self';"
-            "font-src 'self';"
-            "form-action 'self';"
-            "frame-ancestors 'self';"
-            "img-src 'self';"
-            "object-src 'none';"
-            "script-src 'nonce-{0}';"
-            "style-src 'nonce-{0}';"
-            //"upgrade-insecure-requests"
-            , nonceStr));
 }
 
 void Insound::Helmet::after_handle(crow::request &req, crow::response &res,
     context &ctx)
 {
+    static bool USE_SSL = requireEnv("HOST_ADDRESS").starts_with("https");
 
+    auto &headers = ctx.headers;
+
+    // set all headers
+    res.add_header("Strict-Transport-Security",
+        headers.strictTransportSecurity.value_or(
+            "max-age=15552000; includeSubDomains"));
+
+    res.add_header("Cross-Origin-Embedder-Policy",
+        headers.crossOriginEmbedderPolicy.value_or("require-corp"));
+
+    res.add_header("Cross-Origin-Opener-Policy",
+        headers.crossOriginOpenerPolicy.value_or("same-origin"));
+
+    res.add_header("Cross-Origin-Resource-Policy",
+        headers.crossOriginResourcePolicy.value_or("same-origin"));
+
+    res.add_header("Cross-Agent-Cluster",
+        headers.crossAgentCluster.value_or("?1"));
+
+    res.add_header("Referrer-Policy",
+        headers.referrerPolicy.value_or("no-referrer"));
+
+    res.add_header("X-Content-Type-Options",
+        headers.xContentTypeOptions.value_or("no-sniff"));
+
+    res.add_header("X-DNS-Prefetch-Control",
+        headers.xDnsPrefetchControl.value_or("off"));
+
+
+    res.add_header("X-Download-Options",
+        headers.xDownloadOptions.value_or("noopen"));
+
+    res.add_header("X-Frame-Options",
+        headers.xFrameOptions.value_or("DENY"));
+
+    res.add_header("X-Permitted-Cross-Domain-Policies",
+        headers.xPermittedCrossDomainPolicies.value_or("none"));
+
+    res.add_header("X-XSS-Protection",
+        headers.xXssProtection.value_or("0"));
+
+    // Add CSP
+    res.add_header("Content-Security-Policy",
+        headers.contentSecurityPolicy.value_or(
+            f(
+                "base-uri 'self';"
+                "font-src 'self';"
+                "form-action 'self';"
+                "frame-ancestors 'none';"
+                "img-src 'self';"
+                "object-src 'none';"
+                "script-src 'nonce-{0}';"
+                "style-src 'nonce-{0}'"
+                "{1}"
+                , ctx.nonce,
+                USE_SSL ? "; upgrade-insecure-requests" : ""
+            )
+        )
+    );
+
+    // upgrade-insecure-requests not 100% necessary if using cloudflare with
+    // auto http -> https upgrades
+    // leave here anyway
 }
--- insound/core/middleware/Helmet.h
+++ insound/core/middleware/Helmet.h
@@ -1,17 +1,77 @@
 #pragma once
 #include <crow/middleware.h>
 
+#include <optional>
+#include <string>
+
 namespace Insound {
 
+    /**
+     * Middleware that injects important security headers into the application.
+     * User may alter the headers in any middleware or route by setting the
+     * Headers object in the context.
+     *
+     * All headers are set as the very last after_handle.
+     */
     class Helmet : crow::ILocalMiddleware
     {
     public:
+        using OptString = std::optional<std::string>;
+
+        /**
+         * Security headers to set. Please check implementation file for
+         * defaults.
+         *
+         * For further reading on what these headers do please check out:
+         * Helmet JS
+         * - https://helmetjs.github.io
+         * Article on Sentrium
+         * - https://www.sentrium.co.uk/labs/application-security-101-http-headers
+         *
+         * Look up each individual header on MDN
+         */
+        struct Headers {
+            OptString strictTransportSecurity;
+            OptString contentSecurityPolicy;
+            OptString crossOriginEmbedderPolicy;
+            OptString crossOriginOpenerPolicy;
+            OptString crossOriginResourcePolicy;
+            OptString crossAgentCluster;
+            OptString referrerPolicy;
+
+            // Default: "no-sniff" prevents the browser from guessing the mime
+            // type apart from what is specified in the Content-Type header.
+            // Attackers can attempt to upload files that trick the browser
+            // into thinking it is a different type and potentially execute
+            // harmful code.
+            OptString xContentTypeOptions;
+            OptString xDnsPrefetchControl;
+            OptString xDownloadOptions;
+
+            // Indicate whether a browser should be allowed to render a page in
+            // a `<frame>`, `<iframe>`, `<embed>`, or `<object>`
+            // Prevent click-jacking by presenting our site's material on other
+            // sites. "DENY" or "SAMEORIGIN"
+            OptString xFrameOptions;
+
+            // Limits data external resources, such as Adobe Flash and PDF
+            // documents, can access on the domain. If you don't set this to
+            // "none", other sites can embed this app's data in their content.
+            OptString xPermittedCrossDomainPolicies;
+
+            // On older browsers, uses faulty cross-site scripting protection
+            // built-in to browsers that should be turned off "0", if possible
+            OptString xXssProtection;
+        };
+
         struct context
         {
+            Headers headers;
             std::string nonce;
         };
 
-        void before_handle(crow::request &req, crow::response &res, context &ctx);
+        void before_handle(crow::request &req, crow::response &res,
+            context &ctx);
 
         void after_handle(crow::request &req, crow::response &res,
                           context &ctx);
