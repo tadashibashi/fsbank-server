917be4a28d62836e4e0825d4c56293309f3bbf3a
Aaron Ishibashi <a.ishibashi.music@gmail.com>
2023-11-01 21:11:07 -0700

    Implement csrf secret

--- insound/server/CsrfCheck.cpp
+++ insound/server/CsrfCheck.cpp
@@ -7,21 +7,18 @@
 namespace Insound {
     void CsrfCheck::before_handle(crow::request &req, crow::response &res, context &ctx)
     {
+        static std::string CSRF_SECRET_KEY{requireEnv("CSRF_SECRET_KEY")};
+
         // Only check for non-get requests. Get requests should not perform any
         // mutable task such as posting data, altering databases, etc.
         if (req.method != crow::HTTPMethod::GET)
         {
             auto csrftoken = req.get_header_value("X-CSRF-TOKEN");
-            if (csrftoken.empty())
-            {
-                res.code = 403;
-                return res.end("Unauthorized.");
-            }
-
             auto &cookies = Server::getContext<crow::CookieParser>(req);
             auto cookie = cookies.get_cookie("csrftoken");
 
-            if (cookie != csrftoken)
+            if (cookie.empty() ||
+                (csrftoken != CSRF_SECRET_KEY && csrftoken != cookie))
             {
                 res.code = 403;
                 return res.end("Unauthorized.");
