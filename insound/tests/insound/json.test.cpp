f6a7b60b29c90f2fd6bb64ca2c10ab2af44ef49c
Aaron Ishibashi <a.ishibashi.music@gmail.com>
2023-11-06 20:27:27 -0800

    Add json tests for non-specialized std container types

---
+++ insound/tests/insound/json.test.cpp
@@ -0,0 +1,18 @@
+#include <insound/tests/test.h>
+#include <insound/core/json.h>
+
+#include <map>
+#include <string>
+#include <string_view>
+#include <vector>
+
+using namespace Insound;
+
+TEST_CASE("Check serializing for non-glaze-specialized types")
+{
+    REQUIRE(JSON::stringify("Hello world") == "\"Hello world\"");
+    REQUIRE(JSON::stringify(std::string_view("Hello world")) == "\"Hello world\"");
+    REQUIRE(JSON::stringify(std::vector<int>{1, 2, 3, 4 ,5}) == "[1,2,3,4,5]");
+    REQUIRE(JSON::stringify(std::list<int>{1, 2, 3, 4 ,5}) == "[1,2,3,4,5]");
+    REQUIRE(JSON::stringify(std::map<std::string, int>{{"hello", 1}, {"world", 2}}) == R"({"hello":1,"world":2})");
+}
