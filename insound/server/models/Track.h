28e3ab37c4f1ffddcc2c17be0ae89e74c7775e32
Aaron Ishibashi <a.ishibashi.music@gmail.com>
2023-12-06 01:54:22 -0800

    Create Track mongo document stub

---
+++ insound/server/models/Track.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <map>
+#include <string>
+#include <vector>
+
+namespace Insound
+{
+    class MixPreset
+    {
+    public:
+        std::string name;
+        std::vector<int> levels;
+    };
+
+    struct TrackChannel
+    {
+        
+    };
+
+    class Track
+    {
+    public:
+        Track() = default;
+
+        std::string title;
+
+        // Whether or not this track should loop
+        bool isLooping;
+
+        // Sample position of loop start
+        unsigned long long loopStart;
+        // Sample position of loop end
+        unsigned long long loopEnd;
+
+        // name of each original file, used for retrieving and saving files
+        std::vector<std::string> channels;
+    };
+}
